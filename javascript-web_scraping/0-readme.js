84e7d90da65b338209acd2771a9a4a9914459699
truuue <noahvernhet@gmail.com>
2023-09-20 06:20:12 -0700

    semistandar fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-    } else {
-        console.log(data);
-    }
+  } else {
+    console.log(data);
+  }
 });
