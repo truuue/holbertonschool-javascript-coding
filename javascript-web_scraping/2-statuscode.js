d6e9d306eee804c834a27f81b8c6f27e11da011d
truuue <noahvernhet@gmail.com>
2023-09-20 09:56:12 -0700

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,5 @@ request(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   }
-  else {
-    console.log('code: ' + response.statusCode);
-  }
+  console.log('code: ' + response.statusCode);
 });
