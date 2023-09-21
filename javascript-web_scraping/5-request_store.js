87be26eb7e5efb456fbd38c0a8959bd7699826cd
truuue <noahvernhet@gmail.com>
2023-09-21 01:21:32 -0700

    semistandard fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,12 +5,11 @@ const fs = require('fs');
 request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-        if (err) {
-            console.log(err);
-        }
+      if (err) {
+        console.log(err);
+      }
     });
   }
 });
