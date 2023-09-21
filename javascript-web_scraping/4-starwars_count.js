afc54c02d05787f62f5b5deb1f6b5bb0381d57b0
truuue <noahvernhet@gmail.com>
2023-09-21 01:06:59 -0700

    Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
+const moviesUrl = process.argv[2];
 let count = 0;
-request(process.argv[2], (err, response, body) => {
+request(moviesUrl, (err, response, body) => {
   if (err) {
     console.error(err);
   }
